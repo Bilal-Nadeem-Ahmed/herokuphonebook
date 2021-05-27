3611bb37d70cd135a57ba3a0fe74fa4948174709
Bilal Ahmed <bilal91_@hotmail.com>
2021-05-27 10:42:49 +0100

    added linting

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 
 require('dotenv').config()
 const { request, response } = require('express')
@@ -8,7 +9,7 @@ app.use(express.json())
 const morgan=require('morgan')
 const Person=require('./models/person')
 
-morgan.token('data',(request,response)=>JSON.stringify(request.body))
+morgan.token('data',(request,response) => JSON.stringify(request.body))
 
 app.use(morgan(':method :url :status :response-time ms  :data'))
 // cors
@@ -18,99 +19,99 @@ app.use(cors())
 
 
 
-app.get('/api/persons',(request,response)=>{
-    Person.find({}).then(person=>{
-        response.json(person)
-        
-    }).catch(err=>console.log(err))
+app.get('/api/persons',(request,response) => {
+  Person.find({}).then(person => {
+    response.json(person)
+
+  }).catch(err => console.log(err))
 })
 
-app.get('/info',(request,response)=>{
-        
-    response.send(`<p> Phonebook has info for ${Person.length} people</p>
+app.get('/info',(request,response) => {
+
+  response.send(`<p> Phonebook has info for ${Person.length} people</p>
     <p>${new Date()}</p>`)
 })
-app.get('/api/persons/:id',(request,response,next)=>{
-    
-    Person.findById(request.params.id)
-    .then(person=>{
-        if(person){
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-        
+app.get('/api/persons/:id',(request,response,next) => {
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if(person){
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+
     })
-    .catch(error=>{
-        next(error)
+    .catch(error => {
+      next(error)
     })
 })
-app.delete('/api/persons/:id',(request,response, next)=>{
+app.delete('/api/persons/:id',(request,response, next) => {
 
   Person.findByIdAndRemove(request.params.id)
-  .then(result=>{
+    .then(result => {
       response.status(204).end()
-  }).catch(error=>next(error))  
-  
+    }).catch(error => next(error))
+
 })
 
 
 
-app.post('/api/persons',(request,response,next)=>{
+app.post('/api/persons',(request,response,next) => {
 
-    const body = request.body
-  
-    if(body.name === undefined){
-        return response.status(400).json({error:'name missing'})
-    } 
-    const person = new Person ({
-        name: body.name,
-        number: body.number 
-    })
+  const body = request.body
 
-    person.save().then(savedPerson=>{
-        response.json(savedPerson)
-    }).catch(error=>{
-        if(error.name==='ValidationError'){
-           return response.status(400).json({error: 'name or number too short'})
-        }else {
-            console.log(error)
-        }
-      
-    }) 
-       
-  
-})
-app.put('/api/persons/:id',(request,response,next)=>{
-    const body = request.body
+  if(body.name === undefined){
+    return response.status(400).json({ error:'name missing' })
+  }
+  const person = new Person ({
+    name: body.name,
+    number: body.number
+  })
 
-    const person = {
-        name: body.name,
-        number: body.number
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  }).catch(error => {
+    if(error.name==='ValidationError'){
+      return response.status(400).json({ error: 'name or number too short' })
+    }else {
+      console.log(error)
     }
 
-    Person.findByIdAndUpdate(request.params.id,person,{new:true})
-    .then(updatedPerson=>{
-        response.json(updatedPerson)
+  })
+
+
+})
+app.put('/api/persons/:id',(request,response,next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id,person,{ new:true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
     })
-    .catch(error=>next(error))
+    .catch(error => next(error))
 })
 // error handling middleware for next
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
-  
-  // this has to be the last loaded middleware.
-  app.use(errorHandler)
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 
 const PORT=process.env.PORT
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
+app.listen(PORT,() => {
+  console.log(`server is running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -6,24 +6,24 @@ const url = process.env.MONGODB_URI
 console.log(`connected to ${url}`)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then(result=>{
-    console.log(`connected to Mongodb`)
-})
-.catch((error)=>{
+  .then(result => {
+    console.log('connected to Mongodb')
+  })
+  .catch((error) => {
     console.log('error connecting to mongodb:' , error.message)
-})
+  })
 
 const personSchema = new mongoose.Schema({
-    name: { type:String, required: true, unique:true, minLength: 3},
-    number: { type:String, required: true, unique:true, minLength: 8},
-    
-  })
+  name: { type:String, required: true, unique:true, minLength: 3 },
+  number: { type:String, required: true, unique:true, minLength: 8 },
+
+})
 personSchema.plugin(uniqueValidator)
 personSchema.set('toJSON',{
-    transform:(document,returnedObject)=>{
-        returnedObject.id= returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform:(document,returnedObject) => {
+    returnedObject.id= returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 module.exports = mongoose.model('Person', personSchema)
